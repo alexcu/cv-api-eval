442da219e4394c28145bc7b7fd90f0b2485ad964
Alex Cummaudo <alexcu@me.com>
2018-11-20 11:34:12 +1100

    Include timestamps and duration in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+convertsecs() {
+ ((h=${1}/3600))
+ ((m=(${1}%3600)/60))
+ ((s=${1}%60))
+ printf "%02d:%02d:%02d\n" $h $m $s
+}
+
 mkdir -p csv/aws csv/googlecloud csv/azure
 mkdir -p log/aws log/googlecloud log/azure
 
@@ -23,14 +30,39 @@ azure_temp=$(mktemp)
 azure_final=csv/azure/${run_ts}_$src_images_type.csv
 azure_all=csv/azure/_all_$src_images_type.csv
 
+start_time="$(date -u +%s)"
+start_time_fmt=$(date $T)
+
+date $T
 echo Running AWS Rekognition on $src_images...
+aws_start_time="$(date -u +%s)"
+aws_start_time_fmt=$(date $T)
 python3 aws/aws.py $src_images > $aws_temp
+aws_end_time="$(date -u +%s)"
+aws_end_time_fmt=$(date $T)
+aws_elapsed_seconds="$(($aws_end_time-$aws_start_time))"
 
+date $T
 echo Running Google Cloud Vision on $src_images...
+google_start_time="$(date -u +%s)"
+google_start_time_fmt=$(date $T)
 python3 googlecloud/googlecloud.py $src_images > $google_temp
+google_end_time="$(date -u +%s)"
+google_end_time_fmt=$(date $T)
+google_elapsed_seconds="$(($google_end_time-$google_start_time))"
 
+date $T
 echo Running Azure Computer Vision on $src_images...
+azure_start_time="$(date -u +%s)"
+azure_start_time_fmt=$(date $T)
 python3 azure/azure.py $src_images > $azure_temp
+azure_end_time="$(date -u +%s)"
+azure_end_time_fmt=$(date $T)
+azure_elapsed_seconds="$(($azure_end_time-$azure_start_time))"
+
+end_time="$(date -u +%s)"
+end_time_fmt=$(date $T)
+elapsed_seconds="$(($end_time-$start_time))"
 
 cat > $mail_temp << EOF
 Subject: Vision API Test - Success
@@ -46,6 +78,22 @@ Success with count of rows inferred:
 AWS:	$(wc -l < $aws_temp)
 Google:	$(wc -l < $google_temp)
 Azure:	$(wc -l < $azure_temp)
+
+AWS Time Start:	$aws_start_time_fmt
+AWS Time End:	$aws_end_time_fmt
+AWS Duration:	$(convertsecs $aws_elapsed_seconds)
+
+Google Time Start:	$google_start_time_fmt
+Google Time End:	$google_end_time_fmt
+Google Duration:	$(convertsecs $google_elapsed_seconds)
+
+Azure Time Start:	$azure_start_time_fmt
+Azure Time End:	$azure_end_time_fmt
+Azure Duration:	$(convertsecs $azure_elapsed_seconds)
+
+Total Time Start:	$start_time_fmt
+Total Time End:	$end_time_fmt
+Total  Duration:	$(convertsecs $elapsed_seconds)
 
 --MULTIPART-MIXED-BOUNDARY
 Content-Type: text/csv
